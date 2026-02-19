f1fdafc5da1f9a13e254ea9e55442ae5f76c7d0a
Joshua Choong <choong@uoguelph.ca>
2026-02-18 21:50:31 -0500

    prod deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 
 export default function Home() {
-	return (
+	return ( 
 		<div className="w-full min-h-screen bg-white">
 			<Hero />
 		</div>
 	);
+	// snooping around for fun, not for profit
 }
